a175afa8b7f1672e8998724568ab9b8252004065
Anastasiia Kornilova <kornilova.anastasiia@gmail.com>
2018-01-26 18:43:29 +0300

    Library iniatialization was added.

--- Application/src/main/java/com/example/android/camera2video/CameraActivity.java
+++ Application/src/main/java/com/example/android/camera2video/CameraActivity.java
@@ -21,6 +21,11 @@ import android.os.Bundle;
 
 public class CameraActivity extends Activity {
 
+    static {
+        System.loadLibrary("native-lib");
+        System.loadLibrary("opencv_java3");
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
